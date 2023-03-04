8d01540c3598dfca73f51e1e109b89a156472c76
bhsohn <hanmuson1@gmail.com>
2023-03-04 21:52:45 +0900

    async/await 테스트

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,11 +2,40 @@
 
 namespace ConsoleApp1 // Note: actual namespace depends on the project name.
 {
+    internal interface IA
+    {
+        public int Mail { get; set; }
+
+    }
+    public class AA : IA
+    {
+        public int Mail { get; set; }
+    }
     internal class Program
     {
+        private static async Task countDown(string name)
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{name}:{i}");
+                if (name == "a")
+                {
+                    await Task.Delay(1000);
+                }
+                else
+                {
+                    await Task.Delay(2000);
+                }
+                
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var a = countDown("a");
+            var b = countDown("b");
+            Task.WaitAll(a, b);
+            Console.WriteLine("done");
+            Task.Delay(100000);
         }
     }
 }
