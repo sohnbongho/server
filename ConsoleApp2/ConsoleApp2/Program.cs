78e0ea64a4e535442ec59cd56cfbd01c58e26310
bhsohn <hanmuson1@gmail.com>
2023-03-11 22:43:54 +0900

    auto fec 추가

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,12 +1,90 @@
 ﻿using System;
+using Autofac;
 
 namespace ConsoleApp2 // Note: actual namespace depends on the project name.
 {
-    internal class Program
+    // This interface helps decouple the concept of
+    // "writing output" from the Console class. We
+    // don't really "care" how the Write operation
+    // happens, just that we can write.
+    public interface IOutput
     {
-        static void Main(string[] args)
+        void Write(string content);
+    }
+
+    // This implementation of the IOutput interface
+    // is actually how we write to the Console. Technically
+    // we could also implement IOutput to write to Debug
+    // or Trace... or anywhere else.
+    public class ConsoleOutput : IOutput
+    {
+        public void Write(string content)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(content);
         }
     }
+    
+    // This interface decouples the notion of writing
+    // a date from the actual mechanism that performs
+    // the writing. Like with IOutput, the process
+    // is abstracted behind an interface.
+    internal interface IDateWriter
+    {
+        void WriteDate();
+    }
+
+    // This TodayWriter is where it all comes together.
+    // Notice it takes a constructor parameter of type
+    // IOutput - that lets the writer write to anywhere
+    // based on the implementation. Further, it implements
+    // WriteDate such that today's date is written out;
+    // you could have one that writes in a different format
+    // or a different date.
+    public class TodayWriter : IDateWriter
+    {
+        private IOutput _output;
+        public TodayWriter(IOutput output)
+        {
+            this._output = output;
+        }
+
+        public void WriteDate()
+        {
+            this._output.Write(DateTime.Today.ToShortDateString());
+        }
+    }
+    
+    internal class Program
+    {
+        private static IContainer Container { get; set; }
+
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleOutput>().As<IOutput>();
+            builder.RegisterType<TodayWriter>().As<IDateWriter>();
+            Container = builder.Build();
+
+            // The WriteDate method is where we'll make use
+            // of our dependency injection. We'll define that
+            // in a bit.
+            WriteDate();
+        }
+        public static void WriteDate()
+        {
+            // Create the scope, resolve your IDateWriter,
+            // use it, then dispose of the scope.
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var writer = scope.Resolve<IDateWriter>();
+                writer.WriteDate();
+                
+                var output = scope.Resolve<IOutput>();
+                output.Write("test");
+            }
+        }
+    }
+
+    
+    
 }
---
+++ ConsoleApp2/ConsoleApp2/ReadMe.md
@@ -0,0 +1,3 @@
+﻿
+사용 라이브러리
+https://autofac.readthedocs.io/en/latest/getting-started/index.html
