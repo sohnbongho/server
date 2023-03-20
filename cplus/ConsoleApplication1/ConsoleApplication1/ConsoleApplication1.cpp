33b197f1b0dc366df0e6378a6fc25eecd7a18bb1
bhsohn <hanmuson1@gmail.com>
2023-03-20 21:18:11 +0900

    constructor factory

--- cplus/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ cplus/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,29 +1,18 @@
-﻿// ConsoleApplication1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-#include <cstdio>
-#include <iostream>
-#include <algorithm>
+﻿
+#include "ShapeFactory.h"
 
-using std::max;
-using std::min;
 
-int main()
-{
-    int n1 = std::max(10, 20);
-    int n2 = std::min(10, 20);
 
-    printf("%d %d\n", n1, n2);
+int main() {    
 
-    std::cout << "Hello World!\n";
+    // 함수 포인터를 이용한 생성자 팩토리 사용
+    auto instance = ShapeFactory::getInstance();
+
+	auto rect = instance->createShape("Rectangle");
+    rect->draw();
+
+    auto circle = instance->createShape("Circle");
+    circle->draw();
+
+    return 0;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
