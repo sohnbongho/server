04584360e122ea501a2689a6a741a4e687d98e9d
bhsohn <hanmuson1@gmail.com>
2023-03-28 20:58:58 +0900

    obj pool

---
+++ CPP_Server/Server/Document/Object Pool_소스코드/GameServer/GameServer.cpp
@@ -0,0 +1,60 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+#include <atomic>
+#include <mutex>
+#include <windows.h>
+#include <future>
+#include "ThreadManager.h"
+
+#include "RefCounting.h"
+#include "Memory.h"
+#include "Allocator.h"
+
+class Knight
+{
+public:
+	int32 _hp = rand() % 1000;
+};
+
+class Monster
+{
+public:
+	int64 _id = 0;
+};
+
+int main()
+{
+	Knight* knights[100];
+
+	for (int32 i = 0; i < 100; i++)
+		knights[i] = ObjectPool<Knight>::Pop();
+
+	for (int32 i = 0; i < 100; i++)
+	{
+		ObjectPool<Knight>::Push(knights[i]);
+		knights[i] = nullptr;
+	}
+	
+	shared_ptr<Knight> sptr = ObjectPool<Knight>::MakeShared();
+	shared_ptr<Knight> sptr2 = MakeShared<Knight>();
+	
+	for (int32 i = 0; i < 5; i++)
+	{
+		GThreadManager->Launch([]()
+			{
+				while (true)
+				{
+					Knight* knight = xnew<Knight>();
+
+					cout << knight->_hp << endl;
+
+					this_thread::sleep_for(10ms);
+
+					xdelete(knight);
+				}
+			});
+	}
+
+	GThreadManager->Join();
+}
---
+++ CPP_Server/Server/Document/Object Pool_소스코드/ServerCore/CorePch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Types.h"
+#include "CoreMacro.h"
+#include "CoreTLS.h"
+#include "CoreGlobal.h"
+#include "Container.h"
+
+#include <windows.h>
+#include <iostream>
+using namespace std;
+
+#include "Lock.h"
+
+#include "ObjectPool.h"
