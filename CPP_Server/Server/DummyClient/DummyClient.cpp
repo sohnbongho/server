78870b75148064137658b621f71c2b6afbd8abd9
bhsohn <hanmuson1@gmail.com>
2023-03-30 11:28:15 +0900

    socket #2

--- CPP_Server/Server/DummyClient/DummyClient.cpp
+++ CPP_Server/Server/DummyClient/DummyClient.cpp
@@ -1,9 +1,63 @@
-﻿// DummyClient.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-#include "pch.h"
+﻿#include "pch.h"
 #include <iostream>
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
 int main()
-{
+{		
+	WSADATA wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+	
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if(clientSocket == INVALID_SOCKET)
+	{
+		int32 errorCode = ::WSAGetLastError();
+		cout << "Soicket ErrorCode : " << errorCode << endl;
+		return 0;
+	}
+
+	
+	SOCKADDR_IN serverAddr; // IPv4
+	::memset(&serverAddr, 0,sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;	
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+	serverAddr.sin_port = ::htons(7777); // 80 : HTTP
+	
+	if(::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) ==SOCKET_ERROR)
+	{
+		int32 errorCode = ::WSAGetLastError();
+		cout << "Connect ErrorCode : " << errorCode << endl;
+		return 0;
+	}
+
+	//--------------------
+	// 연결 성공: 이제부터 데이터 송수신 가능!
+	cout << "Connected To Server !" << endl;
+
+	while (true)
+	{
+		char sendBuff[100] = "Hello Server!";
+
+		int32 resultCode = ::send(clientSocket, sendBuff, sizeof(sendBuff), 0);
+
+		if(resultCode == SOCKET_ERROR)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Send ErrorCode : " << errCode << endl;
+			return 0;			
+		}
+		
+		this_thread::sleep_for(1s);
+	}
+	//----------------------
+
+	// 소켓 리소스 반환
+	::closesocket(clientSocket);
+
+	// winsocke종료
+	::WSACleanup();
 }
 
--- CPP_Server/Server/GameServer/GameServer.cpp
+++ CPP_Server/Server/GameServer/GameServer.cpp
@@ -3,140 +3,80 @@
 #include "CorePch.h"
 #include "CoreMacro.h"
 #include "ThreadManager.h"
-#include "Memory.h"
-#include "Allocator.h"
 
-using TL = TypeList<class Player, class Mage, class Knight, class Archer>;
-
-class Player
-{	
-	
-public:
-	Player()
-	{
-		INIT_TL(Player)
-	}
-	virtual ~Player()
-	{		
-	}
-	DECLARE_TL
-	
-};
-
-class Knight : public Player
-{	
-public:
-	Knight() {
-		INIT_TL(Knight);
-	}
-	
-};
-
-class Mage : public Player
-{
-public:
-	Mage() {
-		INIT_TL(Mage);
-	}
-
-};
-
-class Archer: public Player
-{
-public:
-	Archer() {
-		INIT_TL(Archer);
-	}
-
-};
-
-class Dog 
-{
-public:
-	Dog() {		
-	}
-
-};
-
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
 int main()
 {
-	//TypeList<Mage, Knight>::Head whoAmI;
-	//TypeList<Mage, Knight>::Tail whoAmI2;
+	// 윈속 초기화(ws2_32 라이브러리 초기화)
+	// 관련 정보가 wsaData에 채워짐
+	WSADATA wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
 
-	//TypeList<Mage, TypeList<Knight, Archer>>::Head whoAmI3; // Mage
-	//TypeList<Mage, TypeList<Knight, Archer>>::Tail::Head whoAmI4; // Knight
-	//TypeList<Mage, TypeList<Knight, Archer>>::Tail::Tail whoAmI5; // Archer
-
-	//int32 len1 = Length<TypeList<Mage, Knight>>::value; // 2
-	//// 제귀적 동작
-	//// 1 + 1 + 1 + 0
-	//int32 len = Length<TypeList<Mage, Knight, Archer>>::value; // 3
-
-	//
-	//TypeAt<TL, 0>::Result whoAmI6; // Mage
-	//TypeAt<TL, 1>::Result whoAmI7; // Knight
-	//TypeAt<TL, 2>::Result whoAmI8; // Archer
-
-	//int32 index1 = IndexOf<TL, Mage>::value; // 0
-	//int32 index2 = IndexOf<TL, Knight>::value; // 1
-	//int32 index3 = IndexOf<TL, Dog>::value; // -1
-
-	//bool canConver1 = Conversion<Player, Knight>::exists; // Player -> Knight 로 변환이 가능한가? (ERROR)
-	//bool canConver2 = Conversion<Knight, Player>::exists; // Knight -> Player 로 변환이 가능한가? (OK)
-	//bool canConver3 = Conversion<Player, Dog>::exists; // Player -> Dog 로 변환이 가능한가? (ERROR
-
+	// ad: Address Family (AF_INET = IPv4, AF_INET6 = IPv6)
+	// type: TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
+	// protocol : 0
+	// return : descriptor
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
 	{
-		Player* player = new Player();
-
-		// 변환 못하는게 맞다!!
-		bool canCast = CanCast<Knight*>(player);
-		Knight* knight = TypeCast<Knight*>(player);
-
-		delete player;
+		int32 errorCode = ::WSAGetLastError();
+		cout << "Soicket ErrorCode : " << errorCode << endl;
+		return 0;
 	}
+
+	// 나의 주소는? (IP주소 : Port주소)-> XX 아파트 YY호
+	SOCKADDR_IN serverAddr; // IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY); 	// 니가 알아서 해줘
+	serverAddr.sin_port = ::htons(7777); // 80 : HTTP
+
+	// 안내원의 폰 개통! 식당의 대표 번호
+	if(::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
 	{
-		Player* player = new Knight();
-
-		// 변환 성공!!!
-		bool canCast = CanCast<Knight*>(player);
-		Knight* knight = TypeCast<Knight*>(player);
-
-		delete player;
+		int32 errorCode = ::WSAGetLastError();
+		cout << "Bind ErrorCode : " << errorCode << endl;
+		return 0;
 	}
-	{
-		shared_ptr<Knight> knight = MakeShared<Knight>();
 
-		shared_ptr<Player> player = TypeCast<Player>(knight);
-		bool canCast= CanCast<Player>(knight);
-		
+	// 영업 시작
+	if(::listen(listenSocket, 10) == SOCKET_ERROR) // backLog: 대기열의 최대 한도
+	{
+		int32 errorCode = ::WSAGetLastError();
+		cout << "listen ErrorCode : " << errorCode << endl;
+		return 0;
 	}
-	{
-		shared_ptr<Player> knight = MakeShared<Knight>();
 
-		shared_ptr<Archer> archer = TypeCast<Archer>(knight);
-		bool canCast = CanCast<Mage>(knight);
+	// ---------------------------
+	while (true)
+	{
+		// listenSocket: 문지기
+		// clientSocket: 패킷을 주고 받을수 있는 소켓
+
+		SOCKADDR_IN clientAddr;
+		::memset(&clientAddr, 0, sizeof(clientAddr));
+		int32 addrLen = sizeof(clientAddr);
+
+		// 대리인이 들고 있는 단말기 
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+
+		if(clientSocket == INVALID_SOCKET)
+		{
+			int32 errorCode = ::WSAGetLastError();
+			cout << "accept ErrorCode : " << errorCode << endl;
+			return 0;
+		}
+		char ipAddress[16];
+		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
+		cout << "Client Connected Ip = " << ipAddress << endl;
 	}
-	
 
-	int a = 0;
-	
-
-	/*for (int32 i = 0; i < 2; i++)
-	{
-		GThreadManager->Launch([]()
-			{
-				while (true)
-				{
-					Knight* knight = xnew<Knight>();
-					cout << knight->_hp << endl;
-					this_thread::sleep_for(10ms);
-					xdelete(knight);					
-				}
-			});
-	}*/
-
-	GThreadManager->Join();
-	
+	// ---------------------------
+	// winsocke종료
+	::WSACleanup();
 }
-
